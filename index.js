cf04a19923b9dfa6e1e135aa2d0676eded183070
gengorow <ty07051312@gmail.com>
2018-12-29 00:50:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
 const SECRET_TOKEN = process.env.TOKEN;
 const Discord = require("discord.js");
 const client = new Discord.Client();
